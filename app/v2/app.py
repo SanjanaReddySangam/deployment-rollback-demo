91c2ec1017a698de2a5a5765c5607fe379c611df
Sanjana Reddy Sangam <117575937+SanjanaReddySangam@users.noreply.github.com>
2025-04-14 10:40:10 +0530

    Initial working version: deployment with rollback

---
+++ app/v2/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    raise Exception("Oops! Something went wrong in v2")
+    return "Hello from Version 2!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
